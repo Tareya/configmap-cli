ae542e4cdadd53289bc703d17cd43cdd765c2efa
Tareya <46636978+Tareya@users.noreply.github.com>
2021-09-10 13:10:17 +0800

    Modify  main logic

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 
 	corev1 "k8s.io/api/core/v1"
@@ -17,34 +18,38 @@ import (
 )
 
 func main() {
-	// namespace := os.Getenv("POD_NAMESPACE")
-	// name := os.Getenv("POD_NAME")
 
-	// filepath := filepath.Join("/data/apps", "")
-
-	// configmap := ConfigmapGenerate(namespace, name, filepath)
-	// ConfigmapCreate(configmap)
+	// get pod information like project name and image version
 	p := PodGet()
 	project, version, err := GetPodInfo(p)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// get necessary fields
 	namespace := os.Getenv("POD_NAMESPACE")
 	filename := strings.Join([]string{project, version}, "-")           // joint the configmap name
 	filepath := filepath.Join("/data/apps", project, "tmp/config.json") // joint the tmp file path
 
 	// fmt.Println(namespace, filename, filepath)
 
-	c := ConfigmapGenerate(namespace, filename, filepath)
+	// judge the configmap exsit status, and create the configmap when not exsits.
+	cl := ConfigmapList(namespace)
+	status := ConfigmapStatus(cl, filename)
 
-	e := ConfigmapCreate(c)
-	if e == nil {
-		log.Fatal(e)
-		return
+	if status == true {
+		fmt.Printf("Configmap %v/%v already exists.", namespace, filename)
 	} else {
-		fmt.Printf("Configmap %v/%v created successfully", namespace, filename)
+		c := ConfigmapGenerate(namespace, filename, filepath)
+		e := ConfigmapCreate(namespace, c)
+		if e == nil {
+			fmt.Println("Create configmap failure.")
+			return
+		} else {
+			fmt.Printf("Configmap %v/%v created successfully", namespace, filename)
+		}
 	}
+
 }
 
 // init the rest client
@@ -64,7 +69,7 @@ func InitClient() *kubernetes.Clientset {
 // load the kubeconfig file
 func LoadConfig() *rest.Config {
 
-	config, err := clientcmd.BuildConfigFromFlags("", "./cluster-admin.conf")
+	config, err := clientcmd.BuildConfigFromFlags("", "")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -72,20 +77,50 @@ func LoadConfig() *rest.Config {
 	return config
 }
 
-
-
-
-// create the configmap
-func ConfigmapCreate(c *corev1.ConfigMap) *corev1.ConfigMap {
+// list the configmaps
+func ConfigmapList(namespace string) *corev1.ConfigMapList {
 	// init the clientset instance
 	client := InitClient()
 
-	// define the api iinterface
+	// define the api interface
+	api := client.CoreV1()
+	opts := metav1.ListOptions{}
+
+	// list configmaps operation
+	configmapList, err := api.ConfigMaps(namespace).List(context.TODO(), opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return configmapList
+}
+
+// judge the configmap exsit status
+func ConfigmapStatus(cl *corev1.ConfigMapList, name string) bool {
+
+	// init the regexp instance
+	re := regexp.MustCompile(name)
+
+	// traverse the configmap list to find the input configmap and return the result
+	for _, configmap := range cl.Items {
+		result := re.MatchString(configmap.Name)
+		if result == false {
+			continue
+		}
+	}
+	return true
+}
+
+// create the configmap
+func ConfigmapCreate(namespace string, c *corev1.ConfigMap) *corev1.ConfigMap {
+	// init the clientset instance
+	client := InitClient()
+
+	// define the api interface
 	api := client.CoreV1()
 	opts := metav1.CreateOptions{}
 
-	namespace := os.Getenv("POD_NAMESPACE")
-
+	// create configmap operation
 	configmapCreate, err := api.ConfigMaps(namespace).Create(context.TODO(), c, opts)
 	if err != nil {
 		log.Fatal(err)
